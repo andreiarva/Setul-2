62ce976e0e6c1b9103e66034bf8fedeae5abf8db
andreiarva <115580916+andreiarva@users.noreply.github.com>
2022-11-30 23:20:23 +0200

    Add files via upload

---
+++ P02/P02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Se da o secventa de n numere. Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive. ");
+            string[] t = Console.ReadLine().Split(' ');
+
+            int nrn = 0;
+            int nr0 = 0;
+            int nrp = 0;
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                int n = int.Parse(t[i]);
+                if (n == 0) nr0++;
+                else if (n < 0) nrn++;
+                else nrp++;
+            }
+            Console.WriteLine("Numere negative :" + " " + nrn +
+                              "Numere egale cu zero :" + " " + nr0 +
+                              "Numere pare :" + " " + nrp);
+        }
+    }
+}
---
+++ P3/P2/P2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Se da o secventa de n numere. " +
+                "Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive. ");
+            int n = int.Parse(Console.ReadLine());
+            int nrn = 0;
+            int nr0 = 0;
+            int nrp = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i < 0) nrn++;
+                if (i == 0) nr0++;
+                if (i % 2 == 0) nrp++;
+            }
+            Console.WriteLine("numere negative:" + " " + nrn + "," +
+                               "numere egale cu zero:" + " " + nr0 + "," + 
+                               "numere pare:" + " " +nrp);
+            Console.ReadKey();
+           
+
+        }
+    }
+}
